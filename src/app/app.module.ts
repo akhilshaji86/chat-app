3f65078b9f833d4189c6d22d6f4a3bf75e8978db
Akhil shaji <akhilshaji86@gmail.com>
2020-03-19 18:27:18 +0530

    basic work flow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { ChatContentComponent } from './chat-content/chat-content.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {UserListComponent} from './user-list/user-list.component';
+import {ChatContentComponent} from './chat-content/chat-content.component';
+import {FormsModule} from "@angular/forms";
+import { ChatHeaderComponent } from './chat-header/chat-header.component';
+import { ChatFooterComponent } from './chat-footer/chat-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
-    ChatContentComponent
+    ChatContentComponent,
+    ChatHeaderComponent,
+    ChatFooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
